1efc73bbc41bad8f48e2b2961f0ac7cbcebba9a6
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-10-17 20:11:25 -0300

    creating project 2

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .red]), center: .center)
+            Button("Show Alert") {
+                    self.showingAlert = true
+                }
+            .foregroundColor(.black)
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
+            }
+        }
     }
 }
 

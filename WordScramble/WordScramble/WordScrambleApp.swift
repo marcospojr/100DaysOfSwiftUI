22ce7dd564657bfb85a7a57284598e1e2283ed89
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-10-26 18:35:04 -0300

    creating project 5

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Marcos Jr on 26/10/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

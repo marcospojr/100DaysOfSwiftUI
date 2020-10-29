97a20000faffb0fd2a612ee00ac990ea241c3d86
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-10-29 17:30:27 -0300

    creating new project - Animations

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Marcos Jr on 29/10/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

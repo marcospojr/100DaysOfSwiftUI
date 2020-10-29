1e1518797456d6d3fc4b3916ea77d82c41fdc683
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-10-29 17:42:48 -0300

    button growing up on tapped

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmout: CGFloat = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+            self.animationAmout += 0.25
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmout)
+        .blur(radius: (animationAmout - 1) * 3)
+        .animation(.default)
     }
 }
 

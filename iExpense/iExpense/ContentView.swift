6c421817a80fb23b9f9a068cb15f504a22ee3c8d
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-02 19:31:27 -0300

    finished day 36

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -18,38 +18,50 @@ struct SecondView: View {
     }
 }
 
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+}
+
 struct ContentView: View {
     @State private var showingSheet = false
-    
+
     @State private var numbers = [Int]()
     @State private var currentNumber = 1
     
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    
     var body: some View {
-//        NavigationView {
-//            VStack {
-//                List {
-//                    ForEach(numbers, id: \.self) {
-//                        Text("\($0)")
-//                    }
-//                    .onDelete(perform: removeRows)
-//                }
-//
-//                VStack(spacing: 30) {
-//                    Button("Add Number") {
-//                        self.numbers.append(self.currentNumber)
-//                        self.currentNumber += 1
-//                    }
-//
-//                    Button("Show sheet") {
-//                        self.showingSheet.toggle()
-//                    }
-//                }
-//                .sheet(isPresented: $showingSheet) {
-//                    SecondView(name: "@marcospojr")
-//                }
-//            }
-//            .navigationBarItems(leading: EditButton())
-//        }
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("\($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+
+                VStack(spacing: 30) {
+                    Button("Add Number") {
+                        self.numbers.append(self.currentNumber)
+                        self.currentNumber += 1
+                    }
+
+                    Button("Show sheet") {
+                        self.showingSheet.toggle()
+                    }
+                    .sheet(isPresented: $showingSheet) {
+                        SecondView(name: "@marcospojr")
+                    }
+                    
+                    Button("Tap count: \(tapCount)") {
+                        self.tapCount += 1
+                        UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+                    }
+                }
+            }
+            .navigationBarItems(leading: EditButton())
+        }
     }
     
     func removeRows(at offsets: IndexSet) {

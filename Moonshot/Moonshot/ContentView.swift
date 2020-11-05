b5a7e3743a1e9e73423177e1ab4bc531c0fad6ac
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-05 19:19:28 -0300

    project 8 start

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+            
+            struct User: Codable {
+                var name: String
+                var address: Address
+            }
+            
+            struct Address: Codable {
+                var street: String
+                var city: String
+            }
+            
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
     }
 }
 

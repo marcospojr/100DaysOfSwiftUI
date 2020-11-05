56a873b9edb7bbbc413930a4aac6fdfa546dd336
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-05 16:46:04 -0300

    creating moonshot

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Marcos Jr on 05/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

2447b82e1dbf80eedaf4ef52132d961475f534e9
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-07 20:00:49 -0300

    complete project 8

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Marcos Jr on 06/11/20.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,38 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Marcos Jr on 07/11/20.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+                    
+                    Text(self.astronaut.description)
+                        .padding()
+                        .layoutPriority(1)
+                }
+            }
+        }
+        .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astrounauts.json")
+    
+    static var previews: some View {
+        AstronautView(astronaut: astronauts[0])
+    }
+}

7090d6a80d50474b9d5eb66509fc9364cff85f3a
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-09 17:23:48 -0300

    creating project 9

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Marcos Jr on 09/11/20.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

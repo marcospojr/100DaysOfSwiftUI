8c14044e56280f0070d85b7a34363bb577356ba1
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-11 19:51:58 -0300

    finished day 45

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,45 +7,114 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
     
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: CGFloat(value))
-                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
-                        self.color(for: value, brightness: 1),
-                        self.color(for: value, brightness: 0.5)
-                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+           AnimatablePair(Double(rows), Double(columns))
+        }
+
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
             }
         }
-        .drawingGroup()
-    }
-    
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(self.steps) + self.amount
-        
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-        
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+
+        return path
     }
 }
 
 struct ContentView: View {
-    @State private var colorCycle = 0.0
-    
+    @State private var amount: CGFloat = 0.0
+//    @State private var insetAmount: CGFloat = 50
+    @State private var rows = 4
+    @State private var columns = 4
+
     var body: some View {
         VStack {
-            ColorCyclingCircle(amount: self.colorCycle)
-                .frame(width: 300, height: 300)
+//            ZStack {
+//                Circle()
+//                    .fill(Color.red)
+//                    .frame(width: 200 * amount)
+//                    .offset(x: -50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+//                    .fill(Color.green)
+//                    .frame(width: 200 * amount)
+//                    .offset(x: 50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+//                    .fill(Color.blue)
+//                    .frame(width: 200 * amount)
+//                    .blendMode(.screen)
+//            }
+//            .frame(width: 300, height: 300)
+//
+//            Slider(value: $amount)
+//                .padding()
             
-            Slider(value: $colorCycle)
+//            Trapezoid(insetAmount: insetAmount)
+//                .background(Color.white)
+//                .frame(width: 200, height: 100)
+//                .onTapGesture {
+//                    withAnimation {
+//                            self.insetAmount = CGFloat.random(in: 10...90)
+//                        }
+//                }
+            
+            Checkerboard(rows: rows, columns: columns)
+                        .onTapGesture {
+                            withAnimation(.linear(duration: 3)) {
+                                self.rows = 8
+                                self.columns = 16
+                            }
+                        }
+            
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 

f7a0fac9e913824b7ce83ef7c39f052d7e359b3d
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-15 22:57:57 -0300

    Validating and disabling forms

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,28 +7,28 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Marcos Pereira"
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-}
-
 struct ContentView: View {
+    @State var username = ""
+    @State var email = ""
+    
+    var disabledForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(disabledForm)
+        }
     }
 }
 

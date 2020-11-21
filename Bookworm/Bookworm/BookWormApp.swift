51a825cdfa4336396e9d5226434ae92e645698e3
Marcos Pereira de Oliveira Junior <marcos.junior@tqi.com.br>
2020-11-21 19:59:00 -0300

    day 55

---
+++ Bookworm/Bookworm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Marcos Jr on 21/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

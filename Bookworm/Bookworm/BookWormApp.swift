050af81f9f1c9e7adac65297a3041a77449e6fab
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-22 15:31:44 -0300

    starting day 56

--- Bookworm/Bookworm/BookWormApp.swift
+++ Bookworm/Bookworm/BookWormApp.swift
@@ -2,16 +2,19 @@
 //  BookwormApp.swift
 //  Bookworm
 //
-//  Created by Marcos Jr on 21/11/20.
+//  Created by Marcos Jr on 19/11/20.
 //
 
 import SwiftUI
 
 @main
 struct BookwormApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }

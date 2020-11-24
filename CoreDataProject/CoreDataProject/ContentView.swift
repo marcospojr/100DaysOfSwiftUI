c1b464edfb878ca9106c7eb5d9a9775702a768fd
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-24 19:27:42 -0300

    Filtering @FetchRequest using NSPredicate

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -5,31 +5,37 @@
 //  Created by Marcos Jr on 24/11/20.
 //
 
+import CoreData
 import SwiftUI
-
-struct Student: Hashable {
-    let name: String
-}
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH %@", "E")) var ships: FetchedResults<Ship>
 
     var body: some View {
-        List(wizards, id: \.self) { wizard in
-            Text(wizard.name ?? "Unkown")
-        }
-        
-        Button("Add") {
-            let wizard = Wizard(context: self.moc)
-            wizard.name = "Harry Potter"
-        }
-        
-        Button("Save") {
-            do {
-                try self.moc.save()
-            } catch {
-                print(error.localizedDescription)
+        VStack {
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            
+            Button("Add Examples") {
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millenium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? self.moc.save()
             }
         }
     }

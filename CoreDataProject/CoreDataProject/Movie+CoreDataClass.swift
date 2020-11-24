39482ef7fa6882b74b8f5e8f457fa41e1eef6915
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-11-24 18:37:07 -0300

    Creating NSManagedObject subclasses

---
+++ CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Marcos Jr on 24/11/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}

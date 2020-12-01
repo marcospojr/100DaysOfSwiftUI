050533b397e62d0beca8c9d13e9464d8ee2508ea
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-12-01 17:34:55 -0300

    finished day 65

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -12,11 +12,24 @@ import SwiftUI
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
+    
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
     
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
     var body: some View {
-        NavigationView {
+        let intensity = Binding<Double>(
+            get: {
+                self.filterIntensity
+            }, set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+
+        return NavigationView {
             VStack {
                 ZStack {
                     Rectangle()
@@ -38,7 +51,7 @@ struct ContentView: View {
                 
                 HStack {
                     Text("Intensity")
-                    Slider(value: $filterIntensity)
+                    Slider(value: intensity)
                 }
                 .padding(.vertical)
                 
@@ -64,7 +77,21 @@ struct ContentView: View {
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 

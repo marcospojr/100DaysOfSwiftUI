478f647a542e250a552e249e790e6d4212ba03c6
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-12-07 18:19:23 -0300

    day 71

---
+++ BucketList/BucketList/MKPointAnnotation-ObservableObject.swift
@@ -0,0 +1,30 @@
+//
+//  MKPointAnnotation-ObservableObject.swift
+//  BucketList
+//
+//  Created by Marcos Jr on 07/12/20.
+//
+
+import MapKit
+
+extension MKPointAnnotation: ObservableObject {
+    public var wrappedTitle: String {
+        get {
+            self.title ?? "Unknown value"
+        }
+        
+        set {
+            title = newValue
+        }
+    }
+    
+    public var wrappedSubtitle: String {
+        get {
+            self.subtitle ?? "Unknown value"
+        }
+        
+        set {
+            subtitle = newValue
+        }
+    }
+}

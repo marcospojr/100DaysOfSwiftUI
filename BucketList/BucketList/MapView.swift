60dab491336be53348eb236e301a0ccd39d33d9e
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-12-08 17:15:23 -0300

    day 72

--- BucketList/BucketList/MapView.swift
+++ BucketList/BucketList/MapView.swift
@@ -70,6 +70,7 @@ extension MKPointAnnotation {
     static var example: MKPointAnnotation {
         let annotation = MKPointAnnotation()
         annotation.title = "London"
+        annotation.subtitle = "Home to the 2012 Summer Olympics."
         annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: -0.13)
         return annotation
     }

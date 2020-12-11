5053945f3bf8e7d53f081e7c06a775178fa27a71
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-12-11 10:15:11 -0300

    added accessibility to GuessTheFlag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,18 +7,35 @@
 
 import SwiftUI
 
+let labels = [
+    "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+    "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+    "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+    "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+    "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+    "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+    "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+    "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+    "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+    "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+    "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+]
+
 extension Image {
-    func flagImage() -> some View {
+    func flagImage(number: Int, countries: [String]) -> some View {
         self
             .renderingMode(.original)
             .clipShape(Capsule())
             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
             .shadow(color: .black, radius: 2 )
+            .accessibility(label: Text(labels[countries[number], default: "Unknown flag"]))
    }
 }
 
 struct ContentView: View {
+
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var score = 0
     
@@ -58,7 +75,7 @@ struct ContentView: View {
                         self.flagTapped(number)
                     }) {
                         Image(self.countries[number])
-                            .flagImage()
+                            .flagImage(number: number, countries: countries)
                     }
                     .animation(Animation.default.repeatCount(5).speed(3))
                     .opacity(number == correctAnswer ? 1 : opacityAmount )

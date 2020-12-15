885ad2b03d638fa9e3fe49e32ee5dbe8ef15a41d
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-12-15 18:01:21 -0300

    initial commit day 79

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Marcos Jr on 15/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

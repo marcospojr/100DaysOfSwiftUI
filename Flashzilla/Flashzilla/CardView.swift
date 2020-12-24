60b8c976b527a4e0c429126e2d2cf31e80a4a2c2
Marcos Pereira de Oliveira Junior <marcospojr13@gmail.com>
2020-12-24 20:22:34 -0300

    day 088

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
     
     @State private var isShowingAnswer = true
+    @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -33,6 +35,23 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    self.offset = gesture.translation
+                }
+
+                .onEnded { _ in
+                    if abs(self.offset.width) > 100 {
+                        self.removal?()
+                    } else {
+                        self.offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             self.isShowingAnswer.toggle()
         }
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,16 +9,44 @@ import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
+    @State private var cards = [Card](repeating: Card.example, count: 10)
 
     var body: some View {
-        CardView(card: Card.example)
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: self.cards[index]) {
+                           withAnimation {
+                               self.removeCard(at: index)
+                           }
+                        }
+                        .stacked(at: index, in: self.cards.count)
+                    }
+                }
+            }
+        }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
     }
 }
